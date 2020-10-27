df98373422c579105c9148bd93e876652f8eca77
Alexandre Morande <c.alexandre.morande@dxc.com>
2020-10-26 23:15:06 -0300

    Update pipeline1.groovy

--- pipeline/pipeline1.groovy
+++ pipeline/pipeline1.groovy
@@ -2,7 +2,6 @@ if (params.Continuar != 'Sim') {
 	currentBuild.result = 'UNSTABLE'
 } else {
 	node {
-		def String[] var_arquivarEstag = ['Estagio 1.txt','Estagio 2.txt','Estagio 2.1.txt','Estagio 2.2.txt','Estagio 2.3.txt','Estagio 3.txt']
         def nomeEstagio = ''
 		def seqEstagio = 1
 
@@ -40,12 +39,7 @@ if (params.Continuar != 'Sim') {
                 echo error.toString()
                 currentBuild.result = 'FAILURE'
             }
-            for (int i = 0; i < var_arquivarEstag.size(); i++) {
-                def encontrouEstag = fileExists var_arquivarEstag[i]
-                if (encontrouEstag) {
-                    archiveArtifacts artifacts: var_arquivarEstag[i]
-                }
-            }
+			archiveArtifacts '*.txt'
         }
 	}
 }
